13c581e4ee4f973537f360f8c9e95ce025b50e1c
Vinh Ngu <vinh-ngu@hotmail.com>
2019-02-21 21:44:15 +0100

    Put selenium_hub_host variable in secret env.

--- bot/scripts/crawler.py
+++ bot/scripts/crawler.py
@@ -1,3 +1,4 @@
+import os
 import json
 import platform
 
@@ -18,8 +19,8 @@ class MyWebdriver:
             options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
             self.driver = webdriver.Chrome(chrome_options=options)
         else:
-            selenium_hub_host = "http://grapefruit.quving.com:4444"
-            selenium_hub_url = selenium_hub_host + "/wd/hub"
+            selenium_hub_host = os.getenv("SELENIUM_HUB_HOST")
+            selenium_hub_url = "http://" + selenium_hub_host + "/wd/hub"
             self.driver = webdriver.Remote(command_executor=selenium_hub_url,
                                            desired_capabilities={'browserName': 'firefox'})
         self.driver.implicitly_wait(10)
