90766509f820d79334c4df88fa6554a2cf2f9c23
Vinh Ngu <vinh-ngu@hotmail.com>
2019-02-25 23:44:31 +0100

    Add more sleep-timer.

--- bot/services/stacker.py
+++ bot/services/stacker.py
@@ -24,13 +24,13 @@ class Stacker(Interactor):
                 if next_btn.get_attribute("class") == 'on':
                     next_btn.click()
 
-                    time.sleep(1)
+                    time.sleep(3)
                     self.driver.find_element_by_css_selector('.coords #position').send_keys(planet_main.coord.z)
                     self.driver.find_element_by_css_selector('.coords #system').send_keys(planet_main.coord.y)
                     x = self.driver.find_element_by_css_selector('.coords #galaxy')
                     x.clear()
                     x.send_keys(planet_main.coord.x)
-                    time.sleep(1)
+                    time.sleep(3)
 
                 # Continue if possible
                 next_btn = self.driver.find_element_by_css_selector('#steps #continue')
@@ -38,7 +38,7 @@ class Stacker(Interactor):
                     next_btn.click()
                     self.driver.find_element_by_css_selector('#missionButton3').click()
                     self.driver.find_element_by_css_selector('#allresources').click()
-                    time.sleep(1)
+                    time.sleep(3)
 
                 # Send if possible
                 next_btn = self.driver.find_element_by_css_selector('#start')
