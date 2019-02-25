65065e5d43bd03c122351fc75b93bc4fc30ba346
Vinh Ngu <vinh-ngu@hotmail.com>
2019-02-26 00:28:30 +0100

    Fix entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 celery -A ogamebot worker -l info -B >> celery.log 2>&1 &
 
 echo 'yes' | python manage.py collectstatic --clear
-gunicorn -c gunicorn_config.py sharelog.wsgi
+gunicorn -c gunicorn_config.py ogamebot.wsgi
