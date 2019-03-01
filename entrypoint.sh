bf4017a3e755dc2fc3524f5bad4c90a0b9757f91
Vinh Ngu <vinh-ngu@hotmail.com>
2019-03-01 21:52:09 +0100

    Start nginx-service.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 celery -A ogamebot worker -l info -B >> celery.log 2>&1 &
 
 echo 'yes' | python manage.py collectstatic --clear
-gunicorn -c gunicorn_config.py ogamebot.wsgi
+gunicorn -c gunicorn_config.py ogamebot.wsgi >> gunicorn.log 2>&1 &
+
+cp -r static/ /usr/share/nginx/html
+cp -r nginx.conf /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
